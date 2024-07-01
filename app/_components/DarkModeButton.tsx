4c2502ca4b7c4af737ff0619f79e0bbaf4a3e56f
yusuf <yusufgad24@gmail.com>
2024-07-01 18:23:51 +0300

    Add shadc darkmode toggle

--- app/_components/DarkModeButton.tsx
+++ app/_components/DarkModeButton.tsx
@@ -1,22 +1,45 @@
 "use client";
 
-import moonIcon from "@/public/assets/icon-moon.svg";
-import sunIcon from "@/public/assets/icon-sun.svg";
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function DarkModeButton() {
-  const { setTheme, theme } = useTheme();
+  const { setTheme } = useTheme();
 
   return (
-    <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-      <Image
-        width={24}
-        height={24}
-        src={theme === "light" ? moonIcon : sunIcon}
-        alt={theme === "light" ? "moon icon" : "sun icon"}
-      />
-    </button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className="bg-skin-ebony text-white hover:bg-skin-ebony hover:text-white"
+          variant={"outline"}
+          size="icon"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
 
