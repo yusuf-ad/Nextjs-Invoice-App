702e586431bee0ad21386dff5e3254df4d220ec4
yusuf <yusufgad24@gmail.com>
2024-07-12 19:40:30 +0300

    Seed database

--- components/custom/InvoicesList.tsx
+++ components/custom/InvoicesList.tsx
@@ -1,9 +1,14 @@
 import Invoice from "./Invoice";
 
 function InvoicesList({ invoices }: { invoices: any[] }) {
+  const sortedInvoices = invoices.sort(
+    (a, b) =>
+      new Date(b.paymentDue).getTime() - new Date(a.paymentDue).getTime(),
+  );
+
   return (
     <ul className="space-y-4 py-10">
-      {invoices.map((invoice) => (
+      {sortedInvoices.map((invoice) => (
         <Invoice invoice={invoice} key={invoice.invoiceId} />
       ))}
     </ul>
