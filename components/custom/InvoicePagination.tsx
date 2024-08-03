c6237eba7d539b0c1ee52f3448d0cb4059b3580d
yusuf <yusufgad24@gmail.com>
2024-08-03 20:23:18 +0300

    Add pagination functionality 2

--- components/custom/InvoicePagination.tsx
+++ components/custom/InvoicePagination.tsx
@@ -10,7 +10,7 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import toast from "react-hot-toast";
 
 type InvoicePaginationProps = {
@@ -18,6 +18,8 @@ type InvoicePaginationProps = {
 };
 
 function InvoicePagination({ totalPages }: InvoicePaginationProps) {
+  const [pages, setPages] = useState(1);
+
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -30,54 +32,70 @@ function InvoicePagination({ totalPages }: InvoicePaginationProps) {
 
   function handlePageChange(page: number) {
     params.set("page", page.toString());
-
     router.push(`${pathname}?${params.toString()}`);
   }
 
   function handlePrevious() {
     if (currentPage > 1) {
-      handlePageChange(currentPage - 1);
+      const newPage = currentPage - 1;
+      setPages(Math.max(1, newPage - 2));
+      handlePageChange(newPage);
     }
   }
 
   function handleNext() {
     if (currentPage < totalPages) {
-      handlePageChange(currentPage + 1);
+      const newPage = currentPage + 1;
+      setPages(Math.max(1, newPage - 2));
+      handlePageChange(newPage);
     }
   }
 
   useEffect(() => {
     if (currentPage > totalPages) {
       toast.error("Invalid page number. Redirecting to the last page.");
-
       params.set("page", totalPages.toString());
       router.push(`${pathname}?${params.toString()}`);
     }
-  }, [currentPage, params, pathname, router, totalPages]);
+  }, [currentPage, pathname, router, totalPages, params]);
 
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem className="cursor-pointer" onClick={handlePrevious}>
+        <PaginationItem
+          className="cursor-pointer select-none"
+          onClick={handlePrevious}
+        >
           <PaginationPrevious isActive={currentPage > 1} />
         </PaginationItem>
-        {Array.from({ length: totalPages }).map((_, index) => (
-          <PaginationItem
-            className="cursor-pointer"
-            onClick={handlePageChange.bind("this", index + 1)}
-            key={index}
-          >
-            <PaginationLink isActive={currentPage === index + 1}>
-              {index + 1}
-            </PaginationLink>
-          </PaginationItem>
-        ))}
-        {totalPages > 3 && (
+        {currentPage > 3 && (
           <PaginationItem>
             <PaginationEllipsis />
           </PaginationItem>
         )}
-        <PaginationItem className="cursor-pointer" onClick={handleNext}>
+        {Array.from(
+          { length: Math.min(3, totalPages) },
+          (v, i) => i + pages,
+        ).map((page) => (
+          <PaginationItem
+            className="cursor-pointer"
+            onClick={() => handlePageChange(page)}
+            key={page}
+          >
+            <PaginationLink isActive={currentPage === page}>
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+        {totalPages > 3 && pages + 2 < totalPages && (
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        )}
+        <PaginationItem
+          className="cursor-pointer select-none"
+          onClick={handleNext}
+        >
           <PaginationNext isActive={currentPage < totalPages} />
         </PaginationItem>
       </PaginationContent>
